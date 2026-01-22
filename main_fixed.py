dd702efb6bb377dfda2bb8f41f29e25c3f98ab6b
Evgeniy <nizhnikov.e@gmail.com>
2026-01-22 10:43:50 +0300

    Update and rename main fixed.py to main_fixed.py

--- main_fixed.py
+++ main_fixed.py
@@ -1,7 +1,7 @@
 from transliterate import translit
 from num2words import num2words
-print()
-print()
+
+
 print(translit("Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime and I guess that this is mine.\nPeople have also told me to make these next few minutes escruciatingly embarrassing and to take vengeance of my enemies.\nNeither will happen.", 'ru'))
 print(translit("\nMore than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40. When I was 8...", 'ru'))
 print()
@@ -15,4 +15,5 @@ print(translit(num2words(3), 'ru'))
 print("40 - ", end="")
 print(translit(num2words(40), 'ru'))
 print("8 - ", end="")
+
 print(translit(num2words(8), 'ru'))
